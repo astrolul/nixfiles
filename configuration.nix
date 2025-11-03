24718deb7468df108890c8750ca8b395f9466398
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-03 18:48:01 +0000

    fixed bluetooth codec issue

--- configuration.nix
+++ configuration.nix
@@ -94,6 +94,7 @@
     feh
     pavucontrol
     flashprog
+    pciutils
     inputs.dmenu-src.packages.${pkgs.system}.default
     inputs.slstatus-src.packages.${pkgs.system}.default
     (inputs.st-src.packages.${pkgs.system}.default.overrideAttrs (oldAttrs: rec {
@@ -113,10 +114,17 @@
 
   services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
     "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      "bluez5.codecs" = [ "sbc" "sbc_xq" "aac" "aptx" "aptx_hd" "ldac" ];
+    };
+  };
+
+  services.pipewire.wireplumber.extraConfig."11-bluetooth-policy" = {
+    "wireplumber.settings" = {
+      "bluetooth.autoswitch-to-headset-profile" = false;
     };
   };
 
@@ -174,8 +182,8 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-#    alsa.enable = true;
-#    alsa.support32Bit = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 
