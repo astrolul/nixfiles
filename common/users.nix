25e17a2fd6686ec326c1e1e45a1e73b5d1a7acc6
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-05 22:52:47 +0000

    added public ssh key

--- common/users.nix
+++ common/users.nix
@@ -7,6 +7,12 @@
     packages = with pkgs; [];
   };
 
+  users.users.astrolul.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILJqpwrawF97qG9k5Ss28crZEqUcOGz1mrsncycByksb astrolul" # content of authorized_keys file
+    # note: ssh-copy-id will add user@your-machine after the public key
+    # but we can remove the "@your-machine" part
+  ];
+
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
   environment.variables = {
