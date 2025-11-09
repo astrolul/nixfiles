dbf8f1be01843c8668c4a60bb3f993d4bd86614b
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-09 22:29:33 +0000

    cleaned and tidied home-manager config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,104 +5,98 @@
   home.homeDirectory = "/home/astrolul";
   home.stateVersion = "25.05";
   
-  home.packages = [
-    pkgs.fastfetch
-    pkgs.pfetch
-    pkgs.htop
-    pkgs.cmus
-    pkgs.nerd-fonts.terminess-ttf
-    pkgs.flameshot
-    pkgs.brightnessctl
-    pkgs.rofi-power-menu
-    pkgs.weechat
-    pkgs.streamrip
-    pkgs.hugo
-    pkgs.mpv
-    pkgs.yt-dlp
-    pkgs.statix
-    pkgs.thonny
-    pkgs.gemini-cli
-    pkgs.ungoogled-chromium
-    pkgs.pcmanfm
-    pkgs.xarchiver
-    pkgs.unrar
-    pkgs.nicotine-plus
-    pkgs.lxappearance
+  home.packages = with pkgs; [
+    fastfetch
+    pfetch
+    htop
+    cmus
+    nerd-fonts.terminess-ttf
+    flameshot
+    brightnessctl
+    rofi-power-menu
+    weechat
+    streamrip
+    hugo
+    mpv
+    yt-dlp
+    statix
+    thonny
+    gemini-cli
+    ungoogled-chromium
+    pcmanfm
+    xarchiver
+    unrar
+    nicotine-plus
+    lxappearance
+    gimp3-with-plugins
   ];  
 
-  programs.bash = {
-    enable = true;
-  };
+  programs = {
+    bash = {
+      enable = true;
+    };
+    git = {
+      enable = true;
+      userName = "astrolul";
+      userEmail = "85197489+astrolul@users.noreply.github.com";
+    };
+    cmus = {
+      enable = true;
+      theme = "gruvbox";
+      extraConfig = ''
+        set output_plugin=pulse
+        set show_current_bitrate=true
+        add ~/music
+      '';
+    };
+    fastfetch = {
+      enable = true;
+    };
+    rofi = {
+      enable = true;
+      plugins = [pkgs.rofi-emoji];
+      theme = "/home/astrolul/merah.rasi";
+    };
+    btop = {
+      enable = true;
+      settings = {
+        color_theme = "gruvbox_dark_v2";
+        theme_background = false;
+        truecolor = true;
+      };
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
+      initContent = ''
+        PS1="%B%F{green}''${''${(%):-%n}:0:1}@''${''${(%):-%m}:0:1}%k %B%F{blue}%1~ %# %b%f%k"
 
-  programs.git = {
-    enable = true;
-    userName = "astrolul";
-    userEmail = "85197489+astrolul@users.noreply.github.com";
-  };
+        EDITOR="nvim"
+        SUDO_EDITOR="nvim"      
 
-  programs.cmus = {
-    enable = true;
-    theme = "gruvbox";
-    extraConfig = ''
-      set output_plugin=pulse
-      set show_current_bitrate=true
-      add ~/music
-    '';
-  };
-
-  programs.fastfetch = {
-    enable = true;
-  };  
-
-  programs.rofi = {
-    enable = true;
-    plugins = [pkgs.rofi-emoji];
-    theme = "/home/astrolul/merah.rasi";
-  };
-
-  programs.btop = {
-    enable = true;
-    settings = {
-      color_theme = "gruvbox_dark_v2";
-      theme_background = false;
-      truecolor = true;
+        if [[ $DISPLAY =~ ^:[0-9]+$ ]] && command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+          tmux attach-session -t default || tmux new-session -s default
+        fi
+      '';
+      shellAliases = {
+        ls = "ls -la";
+        mkdir = "mkdir -p";
+        cp = "cp -r";
+      };
+    };
+    tmux = {
+      enable = true;
+      terminal = "tmux-256color";
+      shell = "${pkgs.zsh}/bin/zsh";
+      extraConfig = ''
+        set-option -sa terminal-overrides ",tmux-256color*:Tc"
+        set -g status-style bg=default
+        set -g status-fg "#fabd2f"
+      '';
     };
   };
- 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    history.size = 10000;
-
-    initContent = ''
-      PS1="%B%F{green}''${''${(%):-%n}:0:1}@''${''${(%):-%m}:0:1}%k %B%F{blue}%1~ %# %b%f%k"
-
-      EDITOR="nvim"
-      SUDO_EDITOR="nvim"      
-
-      if [[ $DISPLAY =~ ^:[0-9]+$ ]] && command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-        tmux attach-session -t default || tmux new-session -s default
-      fi
-    '';
-
-    shellAliases = {
-      ls = "ls -la";
-      mkdir = "mkdir -p";
-      cp = "cp -r";
-    };
-  };
-
- programs.tmux = {
-  enable = true;
-  terminal = "tmux-256color";
-  shell = "${pkgs.zsh}/bin/zsh";
-  extraConfig = ''
-    set-option -sa terminal-overrides ",tmux-256color*:Tc"
-    set -g status-style bg=default
-    set -g status-fg "#fabd2f"
-  '';
- };
 
  services.dunst.enable = true;
 
