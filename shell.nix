4467f14210495bb146d1b93be8322d0d5bb161a7
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-10 22:53:49 +0000

    added clear command when starting dev shell

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
   packages = [ pkgs.gcc pkgs.gdb pkgs.gnumake pkgs."pkg-config" pkgs.ncurses ];
 
   shellHook = ''
+    clear
     echo "Welcome to your C development environment."
     echo "GCC version: $(gcc --version | head -n1)"
   '';
