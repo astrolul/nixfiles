1c518bc3ab1611caf7003e9db5e33ad1759303ee
astrolul <85197489+astrolul@users.noreply.github.com>
2025-12-03 23:09:30 +0000

    some changes to x230 config

--- hosts/x230/configuration.nix
+++ hosts/x230/configuration.nix
@@ -35,81 +35,16 @@
 
   hardware.enableAllFirmware = true;
   hardware.bluetooth.enable = true;
-  
+
+  services.tlp.enable = true;
+
   qt = {
     enable = true;
     platformTheme = "gtk2";
     style = "gtk2";
   };
 
-  environment.systemPackages = with pkgs; [
-    (inputs.st-src.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (oldAttrs: rec {
-      configFile = fetchurl {
-        url = "https://raw.githubusercontent.com/astrolul/st/main/config.h";
-        sha256 = "0g2qrp7hvar9wi981x8pnjfgy2cjiyqc15ggmbqbr8n4m6ihkhsl";
-      };
-      postPatch = (oldAttrs.postPatch or "") + ''
-        cp ${configFile} config.h
-        sed -i '/CPPFLAGS =/ s/$/ -DFONT_SIZE=15 -DBORDER_SIZE=15/' config.mk
-      '';
-    }))
-
-    (inputs.slstatus-src.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (oldAttrs: rec {
-      configFile = fetchurl {
-        url = "https://raw.githubusercontent.com/astrolul/slstatus/main/config.h";
-        sha256 = "1ard769mmdf8vxj2g0zdyfv3ghfw7s540cdwqks94snlawf37fnl";
-      };
-      postPatch = (oldAttrs.postPatch or "") + "\n cp ${configFile} config.h";
-    }))
-
-    (inputs.dmenu-src.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (oldAttrs: rec {
-      configFile = fetchurl {
-        url = "https://raw.githubusercontent.com/astrolul/dmenu/main/config.h";
-        sha256 = "1j0qsz8lw4vxv57cdmygyj5bix5vn51c4sb3sfg0c72ywr0pyxmv";
-      };
-       postPatch = (oldAttrs.postPatch or "") + ''
-        cp ${configFile} config.h
-        sed -i '/CPPFLAGS =/ s/$/ -DFONT_SIZE=16 -DBAR_HEIGHT=4/' config.mk
-      '';
-    }))
-
-  ];
-
-  services.xserver = {
-    enable = true;
-    autorun = true;
-    dpi = 100;
-    windowManager.dwm = {
-      enable = true;
-      package = pkgs.dwm.overrideAttrs (oldAttrs: rec {
-        # Use the dwm source from our flake input
-        src = inputs.dwm-src;
-
-        # Fetch our custom config.h from a URL (pin with a fixed sha256)
-        configFile = pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/astrolul/dwm/refs/heads/main/config.h";
-          sha256 = "0i7gr68xprn6j9v803v8rkym3bagxda3amp475kfh3794lyq2dxb";
-        };
-
-        # In postPatch, copy the fetched config into place before building
-        postPatch = (oldAttrs.postPatch or "") + ''
-          cp ${configFile} config.h
-          sed -i '/CPPFLAGS =/ s/$/ -DFONT_SIZE=16 -DGLYPH_SIZE=24 -DBAR_HEIGHT=32 -DGAP_SIZE=4 -DBORDER_SIZE=4 -DICONSIZE=24/' config.mk
-        '';
-
-        # Preserve the required build inputs for dwm
-        buildInputs = with pkgs; [
-          xorg.libX11
-          xorg.libXft
-          xorg.libXinerama
-          imlib2
-          freetype
-          harfbuzz
-          fontconfig
-        ];
-     });
-   };  
-};
+  environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
