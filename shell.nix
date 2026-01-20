3df728c2ccbc40d9ac0fc5cc84a678853a4690b7
astrolul <85197489+astrolul@users.noreply.github.com>
2026-01-20 20:55:39 +0000

    cleanup shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,26 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
-  packages = [ pkgs.gcc pkgs.gdb pkgs.gnumake pkgs."pkg-config" pkgs.ncurses pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.xorg.libXinerama pkgs.flex pkgs.bison pkgs.python3 pkgs.python313Packages.pygame pkgs.python313Packages.discordpy pkgs.sdl3 pkgs.rustc pkgs.cargo ];
+  packages = [
+    pkgs.gcc
+    pkgs.gdb
+    pkgs.gnumake
+    pkgs."pkg-config"
+    pkgs.ncurses
+    pkgs.xorg.libX11
+    pkgs.xorg.libXft
+    pkgs.xorg.libXinerama
+    pkgs.flex
+    pkgs.bison
+    pkgs.python3
+    pkgs.python313Packages.pygame
+    pkgs.python313Packages.discordpy
+    pkgs.sdl3
+    pkgs.rustc
+    pkgs.cargo
+  ];
 
   shellHook = ''
     clear
